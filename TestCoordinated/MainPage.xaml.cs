f295f83b028970960952b0167bb658960082a94e
Brandon <brandonkuenzi@gmail.com>
2020-02-18 22:12:05 -0900

    Initial Creation

---
+++ TestCoordinated/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace TestCoordinated
+{
+    public sealed partial class MainPage : Page
+    {
+        Frame rootFrame = Window.Current.Content as Frame;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void TheButton_Click(object sender, RoutedEventArgs e)
+        {
+            rootFrame.Navigate(typeof(SecondPage), null, new SuppressNavigationTransitionInfo());
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            try
+            {
+                ConnectedAnimationService.GetForCurrentView()?
+                    .PrepareToAnimate("ThumbnailToHeaderAnimation", TheButton);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ TestCoordinated/SecondPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace TestCoordinated
+{
+    public sealed partial class SecondPage : Page
+    {
+        Frame rootFrame = Window.Current.Content as Frame;
+
+        public SecondPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            rootFrame.Navigate(typeof(MainPage), null, new SuppressNavigationTransitionInfo());
+        }
+
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            var _animation = ConnectedAnimationService.GetForCurrentView().GetAnimation("ThumbnailToHeaderAnimation");
+            _animation.Configuration = new BasicConnectedAnimationConfiguration();
+            if (_animation != null)
+            {
+               var isAnimating = _animation.TryStart(Header, new UIElement[] { CoordinatedText1, CoordinatedText2 });
+            }
+        }
+    }
+}
